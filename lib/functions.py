cee4476f18e750501f57329e83849d8c93bcd8ba
denis <denniiskim@gmail.com>
2024-01-05 03:45:48 +0300

    completed assignment

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print('Hello, programmer!')
 
-def greet(name):
-    pass
+def greet(name="guido"):
+    print(f'Hello, {name}!')
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f'Hello, {name}!')
 
 def add(num1, num2):
-    pass
+   return num1 + num2
 
 def halve(number):
-    pass
+    return number / 2
